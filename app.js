82a46c85caac841f9d0c4dc381fcf005f686ce80
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-29 18:09:48 +0530

    working with demo data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
+const chats = require("./data/data");
 
 const app = express();
 
@@ -10,6 +11,15 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server working 🔥");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 module.exports = app;
